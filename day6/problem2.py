5ffc83a45686faa65da51f970e017bc0cbd5deab
Joshua <joshuagchew@gmail.com>
2021-12-09 15:05:14 -0800

    d6p2 soln

--- day6/problem2.py
+++ day6/problem2.py
@@ -1,4 +1,4 @@
-with open("test.txt", "r") as input:
+with open("input.txt", "r") as input:
     counts = input.readline().split(",")
 
 fishCounts = {
@@ -14,43 +14,26 @@ fishCounts = {
 }
 
 for f in counts:
-    fishCounts[f] = fishCounts[f] + 1
+    fishCounts[f] += 1
     
 print(fishCounts)
 
 newCounts = fishCounts.copy()
-for i in range(18):
+for i in range(256):
     print(fishCounts)
     for c in fishCounts:
-        if c == 0:
-            newCounts[8] = fishCounts[c]
+        if c == "0":
+            newCounts["8"] = fishCounts[c]
+            newCounts["6"] = fishCounts[c]
+        elif c == "7":
+            newCounts[str(int(c, 10) - 1)] += fishCounts[c]
         else:
             newCounts[str(int(c, 10) - 1)] = fishCounts[c]
     fishCounts = newCounts.copy()
     for c in fishCounts:
-        fishCounts[c] = newCounts[c]
         newCounts[c] = 0
 sum = 0
 for c in fishCounts:
     sum += fishCounts[c]
 
 print(sum)
-# fish = []
-# for f in counts:
-#     fish.append(int(f, 10))
-# del counts
-
-# for day in range(256):
-#     newCount = 0
-#     for i in range(len(fish)):
-#         if fish[i] == 0:
-#             fish[i] = 6
-#             newCount += 1
-#         else:
-#             fish[i] -= 1
-#     for i in range(newCount):
-#         fish.append(8)
-
-# print(len(fish))
-
-
