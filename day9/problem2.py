40228b5846c8a078b089e6dc83d47b356203dcd2
Joshua <joshuagchew@gmail.com>
2021-12-15 21:06:16 -0800

    d9p2 soln

--- day9/problem2.py
+++ day9/problem2.py
@@ -1,7 +1,7 @@
 import numpy as np
-import pdb
 
-def checkNorth(x,y):
+def checkNorth(point, prev):
+    # if y is not at the top and prev is not above y and 
     if y == 0:
         return True
     else:
@@ -25,42 +25,55 @@ def checkWest(x,y):
 def checkNeighbors(x,y):
     return checkNorth(x,y) and checkEast(x,y) and checkSouth(x,y) and checkWest(x,y)
 
-def calculateRisk(low_points):
-    sum = 0
-    for point in low_points:
-        sum += int(point) + 1
-    return sum
 
+def calculateBasinScore(low_points):
+    hi, mid, lo = 0, 0, 0
+    for point in low_points:
+        currBasin = calculateBasin(point[0], point[1])
+        if currBasin > lo:
+            if currBasin > mid:
+                if currBasin > hi:
+                    lo = mid
+                    mid = hi
+                    hi = currBasin
+                else:
+                    lo = mid
+                    mid = currBasin
+            else:
+                lo = currBasin
+    print(hi, mid, lo)
+    return hi*mid*lo
+
+# point is the current point to count. prev is the point used to reach current point
+def calculateBasin(y, x):
+    track[y, x] = 1
+    if points[y, x] == 9:
+        return 0
+    north, east, south, west = 0, 0, 0, 0
+    if y != 0 and track[y - 1, x] == 0:
+        north = calculateBasin(y - 1, x)
+    if x != points.shape[1] - 1 and track[y, x + 1] == 0:
+        east = calculateBasin(y, x + 1)
+    if y != points.shape[0] - 1 and track[y + 1, x] == 0:
+        south = calculateBasin(y + 1, x)
+    if x != 0 and track[y, x - 1] == 0:
+        west = calculateBasin(y, x - 1)
+    return 1 + north + east + south + west
+    
 
 with open("input.txt", "r") as input:
-    points = np.array(list(input.readline().strip()))
+    points = np.array(list(map(int, list(input.readline().strip()))))
     for line in input:
-        lineAsArray = np.array(list(line.strip()))
-        points = np.vstack((points, lineAsArray))
+        lineIntArr = list(map(int,list(line.strip())))
+        points = np.vstack((points, lineIntArr))
+    track = np.zeros(points.shape)
 
 print(points)
 low_points = []
 
 for y in range(points.shape[0]):
     for x in range(points.shape[1]):
-        # breakpoint()
         if checkNeighbors(x, y):
-            # pdb.set_trace()
-            # breakpoint()
-            low_points.append(points[y,x])
+            low_points.append((y, x))
 
-print(calculateRisk(low_points))
-
-# ideas:
-# classify each point as seen or unseen when searching
-# iterate through every point, ignoring seen
-# if unseen, mark as seen
-# every time a 9 is encountered, halt this iteration and restart start from the left on the next row
-# add each non-9 to current basin
-# seems difficult to determine verticality of basin. QUALIFY STOP CONDITIONS FOR EACH ITERATION:
-    # save rightmost 9 index of previous row
-    # if any non-9 encountered, must continue search
-    # else, if no non-9 encountered before index of rightmost 9 of previous row, STOP
-
-# mark each 9
-# 
+print(calculateBasinScore(low_points))
