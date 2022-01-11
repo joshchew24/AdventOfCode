b7ce687c72ed97a0342ba96e62e07c5559e22004
Joshua <joshuagchew@gmail.com>
2022-01-10 20:34:33 -0800

    nonfunctional bfs approach

--- day12/problem1.py
+++ day12/problem1.py
@@ -3,15 +3,44 @@ from queue import Queue
 def countPaths(adjList):
     return
 
+class Graph:
+
+    def __init__(self, adjList):
+        self.visited = []
+        self.worklist = Queue(maxsize = 0)
+        for adj in adjList["start"]:
+            self.worklist.put(adj)
+
 def bfs(adjList):
     visited = []
     worklist = Queue(maxsize = 0)
+    numPaths = 0
     for adj in adjList["start"]:
         worklist.put(adj)
+    visited.append("start")
+    # breakpoint()
     while not worklist.empty():
         curr = worklist.get()
-        visited.append(curr)
+        # if curr == "A": breakpoint()
+        if curr == "end": 
+            numPaths += 1
+            visited = []
+            continue
+        if curr.islower(): 
+            if curr not in visited:
+                visited.append(curr)
+            else:
+                continue
+        for adj in adjList[curr]:
+            if adj not in visited:
+                worklist.put(adj)
+
+    print(numPaths)
+
+# def func():
     
+def dfs(adjList):
+
 
 
 def main():
@@ -23,7 +52,6 @@ def main():
             elif second not in adjList[first]: adjList[first].append(second)
             if second not in adjList: adjList[second] = [first]
             elif first not in adjList[second]: adjList[second].append(first)
-        print(adjList)
     bfs(adjList)
 
 
