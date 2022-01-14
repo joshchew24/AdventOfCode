f2f17141bc997d53573f3d485735e036a2224a0e
Joshua <joshuagchew@gmail.com>
2022-01-13 17:52:52 -0800

    working implementation for d12p1

--- day12/problem1.py
+++ day12/problem1.py
@@ -1,5 +1,6 @@
 from queue import Queue
 from collections import deque
+from copy import deepcopy
 
 class Graph:
 
@@ -8,18 +9,24 @@ class Graph:
         self.visited = []
         self.worklist = Queue(maxsize = 0)
         self.numPaths = 0
-        # for adj in adjList["start"]:
-        #     self.worklist.put(adj)
 
-    def countPaths(self, curr, visited):
+    def countPaths(self):
+        self.countPath("start", [])
+        print(self.numPaths)
+
+    def countPath(self, curr, visited):
         adjs = self.adjList[curr]
-        stack = deque()
+        # breakpoint()
         if curr == "end":
             self.numPaths += 1
             return
         if curr in visited: 
             return
-
+        for adj in adjs:
+            nextVisited = deepcopy(visited)
+            if curr.islower(): 
+                nextVisited.append(curr)
+            self.countPath(adj, nextVisited)
 
 def bfs(adjList):
     visited = []
@@ -52,14 +59,15 @@ def bfs(adjList):
 
 def main():
     adjList = {}
-    with open("small.txt", "r") as input:
+    with open("input.txt", "r") as input:
         for line in input:
             first, second = line.strip().split("-") 
             if first not in adjList: adjList[first] = [second]
             elif second not in adjList[first]: adjList[first].append(second)
             if second not in adjList: adjList[second] = [first]
             elif first not in adjList[second]: adjList[second].append(first)
-    bfs(adjList)
+    graph = Graph(adjList)
+    graph.countPaths()
 
 
 
