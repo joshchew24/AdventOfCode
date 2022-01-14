37adbe1df6fc9a0b2fa027fa0b4187c3ca248c25
Joshua <joshuagchew@gmail.com>
2022-01-13 21:48:08 -0800

    d13p2 soln

--- day13/problem2.py
+++ day13/problem2.py
@@ -1,45 +1,57 @@
-from copy import deepcopy
+from collections import defaultdict
+from copy import copy
+import numpy as np
+import matplotlib.pyplot as plt
 
-class Graph:
 
-    def __init__(self, adjList):
-        self.adjList = adjList
-        self.numPaths = 0
+def mirror(coords, axis, posn):        
+    coord_to_flip = coords[axis]
+    flipped = False
+    if coord_to_flip > posn:
+        flipped = True
+        flipped_coord = coord_to_flip - 2*(coord_to_flip - posn)
+        coords = (coords[0], flipped_coord) if axis == 1 else (flipped_coord, coords[1])
+    return coords, flipped
 
-    def countPaths(self):
-        self.countPath("start", [], True)
-        print(self.numPaths)
+def get_axis_to_change_and_posn(instr):
+    axis_str, posn = instr.split("=")
+    posn = int(posn)
+    axis_to_change = 0 if axis_str == "x" else 1
+    return axis_to_change, posn
 
-    def countPath(self, curr, visited, canDupe):
-        nextCanDupe = canDupe
-        adjs = self.adjList[curr]
-        if curr == "end":
-            self.numPaths += 1
-            print(visited, curr)
-            return
-        if curr.islower() and curr in visited:
-            if canDupe:
-                nextCanDupe = False
-            else:
-                return
-        if curr == "start" and curr in visited:
-            return
-        for adj in adjs:
-            nextVisited = deepcopy(visited)
-            nextVisited.append(curr)
-            self.countPath(adj, nextVisited, nextCanDupe)
+def print_folded(coords):
+    plt.scatter(*zip(*coords))
+    plt.show()
+
 
 def main():
-    adjList = {}
     with open("input.txt", "r") as input:
+        dots = defaultdict(int)
         for line in input:
-            first, second = line.strip().split("-") 
-            if first not in adjList: adjList[first] = [second]
-            elif second not in adjList[first]: adjList[first].append(second)
-            if second not in adjList: adjList[second] = [first]
-            elif first not in adjList[second]: adjList[second].append(first)
-    graph = Graph(adjList)
-    graph.countPaths()
+            x,y = line.strip('\n').split(",")
+            dot = (int(x),int(y))
+            dots[dot] = 1
+    with open("folds.txt", "r") as instructions:
+        temp_dict = copy(dots)
+        for line in instructions:
+            fold = line.strip("\n")
+            axis, posn = get_axis_to_change_and_posn(fold)
+
+            for dot in dots:
+                new_coords, mirrored = mirror(dot, axis, posn)
+                if mirrored:
+                    temp_dict.pop(dot)
+                    if new_coords not in dots:
+                        temp_dict[new_coords] = 1
+
+            dots = temp_dict
+            temp_dict = copy(dots)
+        
+            coords = list(dots.keys())
+            coords.sort()
+            print(coords)
+    print_folded(list(dots.keys()))
+
 
 if __name__ == "__main__":
     main()
