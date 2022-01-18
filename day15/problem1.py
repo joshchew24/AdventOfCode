b95c9adedee0d7448734e020df0994f6eceda22d
Joshua <joshuagchew@gmail.com>
2022-01-17 18:33:41 -0800

    working on custom heap wrapper and dijsktras

--- day15/problem1.py
+++ day15/problem1.py
@@ -1,6 +1,12 @@
 from collections import defaultdict
-from queue import Queue
+import heapq
 import numpy as np
+
+class PriorityQueue:
+
+    def __init__(self, list):
+        self.queue = list
+        heapq.heapify(queue)
 
 class Graph:
     
@@ -12,30 +18,59 @@ class Graph:
         grid = grid.astype(int)        
         
         self.grid = grid
-        self.visited = np.zeros(grid.shape)
-    
+        self.labelled = np.zeros(grid.shape, dtype=bool)
+        self.distances = np.ones(grid.shape) * np.inf
+        self.predecessors = np.empty(grid.shape, dtype=tuple)
+
     def __str__(self):
         return str(self.grid)
 
-    def bfs(self):
-        worklist = Queue(maxsize = 0)
-        for y, row in enumerate(self.grid):
-            for x, elem in enumerate(row):
-                print(self.neighbours(x, y))
+    def dijkstra(self):
+
+        curr = (0, 0)
+        self.labelled[curr] = True
+        self.distances[curr] = 0
+        self.predecessors[curr] = curr
+
+        worklist = []
+        worklist.append((self.distances[curr], (1, 0)))#########!!!!!!!!!!!!!!!!!!!!!!!!!!
+        worklist.append((1, (0, 1)))
+        heapq.heapify(worklist)
+
+        curr_to_v_edge_weight = self.grid[curr]
+        shortest_path_to_curr = self.distances[curr]
+
+        neighbours = self.neighbours(curr)
+        for v in neighbours:
+            if not v: continue
+            if self.labelled[v]: continue
+            curr_distance_to_v = shortest_path_to_curr + curr_to_v_edge_weight
+            if curr_distance_to_v < self.distances[v]:
+                self.distances[v] = curr_to_v_edge_weight
+            heapq.heappush(worklist, (self.distances[v], v))
+
+        # print(self.labelled)
+        # print(self.distances)
+        # print(self.predecessors)
+        # print(d)
         
-    def neighbours(self, x, y):
+
+
+
+    def neighbours(self, point):
+        x,y = point
         north = (x, y-1) if y > 0 else None
         east = (x+1, y) if x < self.grid.shape[1] - 1 else None
         south = (x, y+1) if y < self.grid.shape[0] - 1 else None
         west = (x-1, y) if x > 0 else None
-        return north, east, south, west
+        return [north, east, south, west]
 
 
 def main():
     with open("small.txt", "r") as input:
         graph = Graph(input)
     print(graph)
-    graph.bfs()
+    graph.dijkstra()
 
 if __name__ == "__main__":
     main()
