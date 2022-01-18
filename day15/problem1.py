2bfa205307f83edec9ce5a74f6f4a55200642994
Joshua <joshuagchew@gmail.com>
2022-01-17 20:57:56 -0800

    solution seems to work but doesn't pass on AOC site. works for test cases

--- day15/problem1.py
+++ day15/problem1.py
@@ -6,15 +6,27 @@ class PriorityQueue:
 
     def __init__(self, list):
         self.queue = list
-        heapq.heapify(queue)
+        heapq.heapify(self.queue)
+    
+    def push(self, elem):
+        heapq.heappush(self.queue, elem)
+
+    def pop(self):
+        return heapq.heappop(self.queue)
+
+    def __str__(self):
+        return str(self.queue)
+
+    def empty(self):
+        return len(self.queue) == 0
 
 class Graph:
     
     def __init__(self, input):
         grid = np.array(list(input.readline().strip()))
         for line in input:
-            lineAsArray = np.array(list(line.strip()))
-            grid = np.vstack((grid, lineAsArray))
+            lineAsarray = np.array(list(line.strip()))
+            grid = np.vstack((grid, lineAsarray))
         grid = grid.astype(int)        
         
         self.grid = grid
@@ -23,6 +35,8 @@ class Graph:
         self.predecessors = np.empty(grid.shape, dtype=tuple)
 
     def __str__(self):
+        return str(self.grid)
+    def __repr__(self):
         return str(self.grid)
 
     def dijkstra(self):
@@ -33,41 +47,56 @@ class Graph:
         self.predecessors[curr] = curr
 
         worklist = []
-        worklist.append((self.distances[curr], (1, 0)))#########!!!!!!!!!!!!!!!!!!!!!!!!!!
-        worklist.append((1, (0, 1)))
-        heapq.heapify(worklist)
-
-        curr_to_v_edge_weight = self.grid[curr]
-        shortest_path_to_curr = self.distances[curr]
-
-        neighbours = self.neighbours(curr)
-        for v in neighbours:
-            if not v: continue
-            if self.labelled[v]: continue
-            curr_distance_to_v = shortest_path_to_curr + curr_to_v_edge_weight
-            if curr_distance_to_v < self.distances[v]:
-                self.distances[v] = curr_to_v_edge_weight
-            heapq.heappush(worklist, (self.distances[v], v))
-
-        # print(self.labelled)
-        # print(self.distances)
-        # print(self.predecessors)
-        # print(d)
+        worklist.append((self.distances[curr], curr))
+        worklist = PriorityQueue(worklist)
         
+
+        while not worklist.empty():
+            # breakpoint()
+            shortest_path_to_curr, curr = worklist.pop()
+            curr_to_v_edge_weight = self.grid[curr]
+            self.labelled[curr] = True
+
+            # if curr == (3,6):
+            #     breakpoint()
+
+            neighbours = self.neighbours(curr)
+            for v in neighbours:
+                # if v == (4,6) or v == (4,7) or v == (3,7):
+                #     breakpoint()
+                if not v: continue
+                if self.labelled[v]: continue
+                curr_distance_to_v = shortest_path_to_curr + curr_to_v_edge_weight
+                if curr_distance_to_v < self.distances[v]:
+                    self.distances[v] = curr_distance_to_v
+                    self.predecessors[v] = curr
+                    worklist.push((self.distances[v], v))
+
+
+        print("Path score: ", self.distances[-1,-1])
+
+        path_tracker = (self.grid.shape[0] - 1, self.grid.shape[1] - 1)
+        path = []
+        while path_tracker != (0,0):
+            path.append(path_tracker)
+            path_tracker = self.predecessors[path_tracker]
+        path.append((0,0))
+        path.reverse()
+        print("Path:", path)
 
 
 
     def neighbours(self, point):
-        x,y = point
-        north = (x, y-1) if y > 0 else None
-        east = (x+1, y) if x < self.grid.shape[1] - 1 else None
-        south = (x, y+1) if y < self.grid.shape[0] - 1 else None
-        west = (x-1, y) if x > 0 else None
+        y,x = point
+        north = (y-1, x) if y > 0 else None
+        east = (y, x+1) if x < self.grid.shape[0] - 1 else None
+        south = (y+1, x) if y < self.grid.shape[1] - 1 else None
+        west = (y, x-1) if x > 0 else None
         return [north, east, south, west]
 
 
 def main():
-    with open("small.txt", "r") as input:
+    with open("input.txt", "r") as input:
         graph = Graph(input)
     print(graph)
     graph.dijkstra()
