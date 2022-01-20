c052926ab376b2f17c4b12c03d9f183f72122f2b
Joshua Chew <joshuagchew@gmail.com>
2022-01-19 19:11:52 -0800

    it's close but doesn't work and is too complicated to debug. going to try to refactor towards a more OOP class-based implementation rather than this more functional-style

--- day16/problem1.py
+++ day16/problem1.py
@@ -1,53 +1,88 @@
+import sys
+
+
 def convert_packet(packet):
+    expected_bin_length = len(packet)*4
     pkt_int = int(packet, base=16)
-    pkt_bin = bin(int(packet, base=16))
+    pkt_bin = bin(int(packet, base=16))[2:]
+    diff = expected_bin_length - len(pkt_bin)
+    while diff:
+        pkt_bin = "0" + pkt_bin
+        diff -= 1
     return pkt_int, pkt_bin
 
 def parse_header_and_body(packet):
+    # breakpoint()
     pkt_ver = int(packet[0:3], base=2)
     pkt_type_id = int(packet[3:6], base=2)
     return pkt_ver, pkt_type_id, packet[6:]
 
+# returns value of literal and final index of literal chunk
 def process_literal(pkt_body):
     result = ""
-    counter = 0
     prefix = 1
-    # can loop through whole list or slice out prefixes until a 0 is encountered, then chop off the trailing bits
-    # for b in pkt_body:
-    #     if counter == 0:
-    #         prefix = int(b, base=2)
-    #     else:
-    #         result += b
-    #     if counter < 4:
-    #         counter += 1
-    #     else:
-    #         counter = 0
-    #         if prefix == 0:
-    #             break
     
     pfx_pointer = 0
     while prefix:
+        breakpoint()
         prefix = int(pkt_body[pfx_pointer], base=2)
         result += pkt_body[pfx_pointer + 1:pfx_pointer + 5]
         pfx_pointer += 5
 
-        
+    return result, 5 + pfx_pointer
 
-    print(result)
-    print(int(result,base=2))
+def process_operands(pkt_body):
+    # breakpoint()
+    pkt_len_id = int(pkt_body[0])
+    sub_pkts_ver_sum = 0
+    total_end_idx = 0
+    if pkt_len_id: # packet-wise length
+        breakpoint()
+        pkt_count = int(pkt_body[1:12], base=2)
+        sub_packets = pkt_body[12:]
+        while pkt_count > 0:
+            breakpoint()
+            ver_sum, end_idx = process_packet(sub_packets)
+            sub_packets = sub_packets[end_idx + 1:]
+            sub_pkts_ver_sum += ver_sum
+            total_end_idx += end_idx
+            pkt_count -= 1
+    else: # bit-wise length
+        bit_count = int(pkt_body[1:16], base=2)
+        sub_packets = pkt_body[16:]
+        while bit_count > 0:
+            ver_sum, end_idx = process_packet(sub_packets)
+            # breakpoint()
+            sub_packets = sub_packets[end_idx + 1:]
+            sub_pkts_ver_sum += ver_sum
+            total_end_idx += end_idx
+            bit_count -= end_idx + 1
+    return sub_pkts_ver_sum, end_idx
 
-
+# return sum of version numbers in packet and subpackets
 def process_packet(packet):
+    # breakpoint()
     pkt_ver, pkt_type_id, pkt_body = parse_header_and_body(packet)
+
+    sum = pkt_ver
+    end_idx = len(packet) - 1
+
     if pkt_type_id == 4:
-        process_literal(pkt_body)
+        literal_value, end_idx = process_literal(pkt_body)
+    else:
+        operands_sum, end_idx = process_operands(pkt_body)
+        sum += operands_sum
+    return sum, end_idx
 
 packet_types = ["","","","","literal"]
     
+inputs = ["input", "small", "bit_length", "pkt_length", "nested", "tree_same", "tree_diff", "nested_big"]
 def main():
-    with open("small.txt", "r") as input:
+    input_idx = int(sys.argv[1])
+    with open(inputs[input_idx] + ".txt", "r") as input:
         pkt_int, pkt_bin = convert_packet(input.readline())
-    process_packet(pkt_bin[2:])
+    print(pkt_bin)
+    print(process_packet(pkt_bin))
 
 if __name__ == "__main__":
     main()
