2260dc78ea2aa3ad0202c724b841ce61f7ec41e0
Joshua Chew <joshchew@students.cs.ubc.ca>
2023-12-11 02:46:37 -0800

    wip day 11

---
+++ 2023/11/1.py
@@ -0,0 +1,64 @@
+import argparse
+import os
+
+import numpy as np
+
+PUZZLE_INPUT = "input.txt"
+EXAMPLE_INPUT = "small.txt"
+
+def main():
+    parser = argparse.ArgumentParser(description='specifying no arguments will run with the puzzle input')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-f', '--file', help='specify an input file to test', default=PUZZLE_INPUT)
+    group.add_argument('-t', '--test', dest='file', action='store_const', const=EXAMPLE_INPUT, help='test the instruction example')
+    parser.add_argument('-p', '--pretty', action='store_true')
+    args = parser.parse_args()
+    in_file = args.file
+    pretty = args.pretty
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, in_file)
+
+    with open(file_path) as input:
+        if pretty:
+            rows = np.array([list(x) for x in input.read().splitlines()])
+        else:
+            rows = []
+            for line in [list(line) for line in input.read().splitlines()]:
+                rows.append([x == "#" for x in line])
+            rows = np.array(rows)
+
+        expanded = np.copy(rows)
+        num_rows_added = 0
+        comparator = "." if pretty else False
+        for i, row in enumerate(rows):
+            if np.all(row == comparator): # TODO: change to false for performance
+                expanded = np.insert(expanded, i + num_rows_added, row, axis=0)
+                num_rows_added += 1
+        cols = expanded.T
+        num_cols_added = 0
+        for i, col in enumerate(cols):
+            if np.all(col == comparator):
+                expanded = np.insert(expanded, i + num_cols_added, col, axis=1)
+                num_cols_added += 1
+        print(expanded)
+        galaxies = list(zip(*np.where(expanded == "#"))) if pretty else list(zip(*np.where(expanded)))
+        print(galaxies)
+        sum = 0
+
+
+        # galaxies = np.array(galaxies, dtype=int)
+        # distances = np.linalg.norm(galaxies[:, np.newaxis, :] - galaxies[np.newaxis, :, :], axis=-1)
+        # print(distances)
+        # for start in galaxies:
+        #     for end in galaxies:
+        #         if start == end:
+        #             continue
+        #         val = (max(end[1], start[1]) - min(end[1], start[1])) + (max(end[0], start[0]) - min(end[0], start[0]))
+        #         print(f"distance from {start} to {end} is {val}")
+        #         sum += val #abs(((end[1] - start[1]) + (end[0] - start[0])))
+        print(sum)
+
+
+
+if __name__ == "__main__":
+    main()
