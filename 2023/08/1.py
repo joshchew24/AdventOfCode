ec9504e96ede96dd010fdda8156ccc7c20535a67
joshchew <joshuagchew@gmail.com>
2023-12-17 15:40:15 -0800

    day 8 part 1

--- 2023/08/1.py
+++ 2023/08/1.py
@@ -1,6 +1,7 @@
 import argparse
 import os
-import sys
+
+from itertools import cycle
 
 PUZZLE_INPUT = "input.txt"
 EXAMPLE_INPUT = "small.txt"
@@ -16,8 +17,22 @@ def main():
     file_path = os.path.join(script_dir, in_file)
 
     with open(file_path) as input:
-        lines = input.readlines()
-        print(lines)
+        directions = cycle([direction == "R" for direction in input.readline().strip()]) # left is false, right is true
+        input.readline()
+        nodes = {}
+        for node in input.readlines():
+            curr, children = node.strip().split(" = ")
+            left, right = children.strip("()").split(", ")
+            child_tuple = (left, right)
+            nodes[curr] = child_tuple
+    curr_node = "AAA"
+    num_moves = 0
+    while curr_node != "ZZZ":
+        direction = next(directions)
+        curr_node = nodes[curr_node][direction]
+        num_moves += 1
+    print(num_moves)
+        
 
 
 
